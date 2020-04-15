fed26c04651e886f5a874b2442101c671a86d6f7
robin-jesson <62558641+robin-jesson@users.noreply.github.com>
2020-04-15 09:12:59 +0200

    signature added to auth jwt

--- Back/auth/src/main/java/com/alten/hercules/auth/service/TokenManager.java
+++ Back/auth/src/main/java/com/alten/hercules/auth/service/TokenManager.java
@@ -5,6 +5,9 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -19,10 +22,19 @@ import io.jsonwebtoken.security.SignatureException;
 
 public class TokenManager {
 	
-	private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-
 	
+	private static final String SECRET_KEY = "7FC2DFF16D23F8914AD318F7156438AB88371C18A2C8BBC7A767DF9B92";
+	private static SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+	/**
+	 * Create a token : header is HS256 algorithm; payload is composed of a role and an expiration time, and signature is SECRET_KEY.
+	 * @param role the role of the authenticated person
+	 * @return token as a string
+	 */
 	public static String createAuthToken(String role) {
+		byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+	    Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+	    
 		Calendar expirationSession = Calendar.getInstance();
 		expirationSession.add(Calendar.HOUR, 4);
 		
@@ -30,13 +42,15 @@ public class TokenManager {
 		payload.put("role", role);
 		payload.put("exp", expirationSession.getTime());
 		
-		return Jwts.builder().setClaims(payload).signWith(key).compact();
+		return Jwts.builder().setClaims(payload).signWith(signatureAlgorithm, signingKey).compact();
+		
+	
 	}
 	
 	public static Claims readAuthToken(String token) {
 		
 		try {
-			return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+			return Jwts.parserBuilder().setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY)).build().parseClaimsJws(token).getBody();
 		} catch (InvalidKeyException e) {  
 			System.err.println("Key error. Error code : " + e.getMessage());
 			e.printStackTrace();
