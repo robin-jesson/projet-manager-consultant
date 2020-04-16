a229856e9a16b606ddb6ac910b44d660658ffe07
robin-jesson <62558641+robin-jesson@users.noreply.github.com>
2020-04-16 09:50:08 +0200

    entité skill ajoutée + table assoc Skill Projet

---
+++ Back/skill/src/main/java/com/alten/hercules/skill/entity/SkillProject.java
@@ -0,0 +1,63 @@
+package com.alten.hercules.skill.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+
+import org.hibernate.annotations.GeneratorType;
+
+@Entity
+public class SkillProject {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("skillId")
+	private Skill skill;
+	
+	private int projectId;
+
+	public SkillProject(int id, Skill skill, int projectId) {
+		super();
+		this.id = id;
+		this.skill = skill;
+		this.projectId = projectId;
+	}
+
+	public SkillProject() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Skill getSkill() {
+		return skill;
+	}
+
+	public void setSkill(Skill skill) {
+		this.skill = skill;
+	}
+
+	public int getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(int projectId) {
+		this.projectId = projectId;
+	}
+	
+	
+	
+}
